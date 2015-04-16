b66a4ad0df5d21fcab97259520089bf6c30a570e
Karen <Karen.Nelligan@students.ittralee.ie>
2015-04-16 12:57:56 +0100

    changed channel to mono and changed sample rate to 8000

--- app/src/main/java/kn/android/com/project/MainActivity.java
+++ app/src/main/java/kn/android/com/project/MainActivity.java
@@ -1,20 +1,18 @@
 package kn.android.com.project;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import android.app.Activity;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
-import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends Activity {
@@ -38,31 +36,30 @@ public class MainActivity extends Activity {
 
         Long tsLong = System.currentTimeMillis()/1000;
 
-        outputFile += "/app/appRecording"+tsLong.toString()+".wmv";
+        outputFile += "/app/appRecording_"+tsLong.toString()+".wmv";
         //end here
 
-        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
 
-        File file = new File(file_path+ "/test.txt");
+
+
+        File file = new File("test.txt");
 
         FileOutputStream fos = null;
-        try{
+        try {
             fos = new FileOutputStream(file);
             fos.write(outputFile.getBytes());
 
-        }catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found" + e);
-        }
-        catch (IOException ioe) {
+        } catch (IOException ioe) {
             System.out.println("Exception while writing file " + ioe);
-        }
-        finally {
+        } finally {
             try {
-                if (fos !=null) {
+                if (fos != null) {
                     fos.close();
                 }
-            }
-            catch (IOException ioe) {
+            } catch (IOException ioe) {
                 System.out.println("Error while closing stream " + ioe);
             }
         }
@@ -71,6 +68,9 @@ public class MainActivity extends Activity {
         myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
         myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setAudioChannels(1);
+        myAudioRecorder.setAudioSamplingRate(8000);
+        myAudioRecorder.setAudioEncodingBitRate(16000);
         myAudioRecorder.setOutputFile(outputFile);
 
     }
